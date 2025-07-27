ae38757bb3fcdaec70c75f6b3663734ab0ac01f4
Urmi khan <157728763+Urmikhan99@users.noreply.github.com>
2025-07-27 21:21:55 +0600

    6-1 Increment Decrement Operator

---
+++ Module 6/6_1_Increment_Decrement_Operator.c
@@ -0,0 +1,37 @@
+// #include<stdio.h>
+// int main() 
+// {
+//      int x=5;
+//      int y=--x;
+    
+//      printf("%d %d",x,y);
+//      return 0;
+// }
+
+// #include<stdio.h>
+// int main() 
+// {
+//      int x=5;
+//      int y=+-x;
+    
+//      printf("%d %d",x,y);
+//      return 0;
+// }
+
+#include<stdio.h>
+int main() 
+{
+     int x=5;
+     int y=++x;
+    
+     printf("%d %d",x,y);
+     return 0;
+}
+
+
+// 🧠 Short Note
+// ✅ Types of Operators
+// Pre-Increment (++x) – Increases the value before using it.
+// Post-Increment (x++) – Uses the value first, then increases.
+// Pre-Decrement (--x) – Decreases the value before using it.
+// Post-Decrement (x--) – Uses the value first, then decreases.
